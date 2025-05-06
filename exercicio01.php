55d6f773319dd6f664b02f0af32c9896d1187deb
AdriTrindade <adriadine.2023301617@aluno.iffar.edu.br>
2025-05-06 10:41:23 -0300

    Add files via upload

--- exercicio01.php
+++ exercicio01.php
@@ -3,27 +3,27 @@
 
 class Produto
 {
-    // propriedades
+    // atributos ou propriedades
     private $nome;
     private $preco;
     private $qtd;
 
+    // Construtor para inicializar as propriedades
     public function __construct($nome, $preco, $qtd) {
         $this->nome = $nome;
         $this->preco = $preco;
         $this->qtd = $qtd;
     }
 
-    // Método
+    // Método para exibir informações
     public function exibeInformacoes() {
-        echo $this->nome.",".
-             $this->preco.",".
-             $this->qtd;
+        echo "Nome: " . $this->nome . ", Preço: " . $this->preco . ", Quantidade: " . $this->qtd;
     }
 
 
 }
 
-$produto = new Produto("Caneta", 5.99, 2);
+// Exemplo de uso
+$produto = new Produto("Caneta Esferográfica Preta", 10.99, 5);
 $produto->exibeInformacoes();
 ?>
